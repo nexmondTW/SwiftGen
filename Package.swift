cdf8bde3859ecb668b366b1ef98ff63c34de45c5
nexmondTW <nexmond@twinny.co.kr>
2021-02-15 15:59:21 +0900

    Update dependency for SPM

--- Package.swift
+++ Package.swift
@@ -9,10 +9,10 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/kylef/Commander.git", from: "0.9.0"),
-        .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.0"),
-        .package(url: "https://github.com/kylef/Stencil.git", from: "0.13.0"),
+        .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.0"),
+        .package(url: "https://github.com/kylef/Stencil.git", from: "0.14.0"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "4.0.0"),
-        .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", from: "2.7.0"),
+        .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", from: "2.8.0"),
         .package(url: "https://github.com/tid-kijyun/Kanna.git", from: "5.2.2")
     ],
     targets: [
